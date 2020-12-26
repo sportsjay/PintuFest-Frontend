4da9e68639f9ccc5a1cf7fa080b32a57f9c0cbbc
Jason Nathaniel <jasonnathaniel1152@gmail.com>
2020-12-26 18:37:43 +0800

    fixed fullscreen problem

--- src/pages/game1.jsx
+++ src/pages/game1.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import '../components/styles.css';
-import BannerImage from '../components/banner_image';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import "../components/styles.css";
+import BannerImage from "../components/banner_image";
 
 export default function Game1() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container id="container">
-        <BannerImage/>
-        <div className="style">Escape Room 1</div> 
-      </Container>
+      <div id="container">
+        <BannerImage />
+        <div className="style">Escape Room 1</div>
+      </div>
     </React.Fragment>
   );
 }
