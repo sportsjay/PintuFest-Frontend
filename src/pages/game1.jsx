b754479c54f9efee7287f1481f491319c05f8bc5
Edward Steven <ewongso001@e.ntu.edu.sg>
2020-12-26 18:59:11 +0800

    removed BannerImage

--- src/pages/game1.jsx
+++ src/pages/game1.jsx
@@ -3,14 +3,13 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import "../components/styles.css";
-import BannerImage from "../components/banner_image";
 
 export default function Game1() {
   return (
     <React.Fragment>
       <CssBaseline />
       <div id="container">
-        <BannerImage />
+        <img src="./images/image_banner.jpg" width="100%"/>
         <div className="style">Escape Room 1</div>
       </div>
     </React.Fragment>
