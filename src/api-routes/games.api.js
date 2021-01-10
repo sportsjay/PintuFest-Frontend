b4e6d1a71603d255cf94f83ff22aafa302eadbe8
Jason Nathaniel <jasonnathaniel1152@gmail.com>
2021-01-11 00:14:25 +0800

    rerouted api-routes

--- src/api-routes/games.api.js
+++ src/api-routes/games.api.js
@@ -1,9 +1,9 @@
-const system_env = "localhost:4000";
+const system_env = "ik2birnfgd.execute-api.ap-southeast-1.amazonaws.com/dev";
 
 export const GAMES_API = {
-  GET_ALL_GAMES: `http://${system_env}/game-api/`,
+  GET_ALL_GAMES: `https://${system_env}/game-api/`,
   GET_GAME_BY_NAME: (name) => {
-    return `http://${system_env}/game-api/${name}`;
+    return `https://${system_env}/game-api/${name}`;
   },
-  BOOK_GAME_SLOT: `http://${system_env}/game-api/book`,
+  BOOK_GAME_SLOT: `https://${system_env}/game-api/book`,
 };
--- src/api-routes/room.api.js
+++ src/api-routes/room.api.js
@@ -1,10 +1,10 @@
-const system_env = "localhost:4000";
+const system_env = "ik2birnfgd.execute-api.ap-southeast-1.amazonaws.com/dev";
 
 export const ROOM_API = {
   UPDATE_ROOM_SLOT_STATUS: (gameRoom) => {
-    return `http://${system_env}/room-api/update-status/${gameRoom}`;
+    return `https://${system_env}/room-api/update-status/${gameRoom}`;
   },
   GET_ROOM_SLOT_STATUS: (gameRoom) => {
-    return `http://${system_env}/room-api/get-status/${gameRoom}`;
+    return `https://${system_env}/room-api/get-status/${gameRoom}`;
   },
 };
